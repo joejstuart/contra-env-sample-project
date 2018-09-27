130d3641e597c2fa5606e6b4b1a1154d8c89e8dd
jstuart <jstuart@redhat.com>
2018-09-27 15:22:31 -0500

    updating job

--- src/jobs/pipelineJobs.groovy
+++ src/jobs/pipelineJobs.groovy
@@ -2,5 +2,5 @@ import org.centos.contra.jobdsl.PipelineJob
 
 
 def job = new PipelineJob(this, 'samplePipelineJob')
-job.addGit([branch: 'master', repoUrl: 'https://github.com/CentOS-PaaS-SIG/contra-env-sample-project.git'])
+job.addGit([branch: 'master', repoUrl: 'https://github.com/joejstuart/dockerImages.git'])
 job.logRotate()
