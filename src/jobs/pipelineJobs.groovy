6a9bddd449bdd7f5f00157a3bfc1af1196b92c9e
jstuart <jstuart@redhat.com>
2018-12-14 10:47:10 -0600

    testing env vars

--- src/jobs/pipelineJobs.groovy
+++ src/jobs/pipelineJobs.groovy
@@ -5,9 +5,13 @@ def components = System.getenv('COMPONENT_JOBS')
 println "printing components"
 println components
 println "done printing"
-def job = new PipelineJob(this, 'samplePipelineJob')
 
-// uncomment if you have fed msg configured
-//job.fedMsgTrigger('org.fedoraprojectb', 'fedora-fedmsg', ['check1': 'value1'])
-job.addGit([repo_branch: 'master', repo_url: 'https://github.com/CentOS-PaaS-SIG/contra-env-sample-project.git'])
-job.logRotate()
+components.split(',') { component ->
+    def job = new PipelineJob(this, component)
+
+    // uncomment if you have fed msg configured
+    //job.fedMsgTrigger('org.fedoraprojectb', 'fedora-fedmsg', ['check1': 'value1'])
+    job.addGit([repo_branch: 'master', repo_url: 'https://github.com/CentOS-PaaS-SIG/contra-env-sample-project.git'])
+    job.logRotate()
+}
+
