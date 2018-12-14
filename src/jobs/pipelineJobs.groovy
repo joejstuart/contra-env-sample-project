f6d84d2bece7414a6d2bd86884ccb203bac6f852
jstuart <jstuart@redhat.com>
2018-12-14 10:09:22 -0600

    updating parameter

--- src/jobs/pipelineJobs.groovy
+++ src/jobs/pipelineJobs.groovy
@@ -3,5 +3,5 @@ import org.centos.contra.jobdsl.PipelineJob
 
 def job = new PipelineJob(this, 'samplePipelineJob')
 job.fedMsgTrigger('org.fedoraprojectb', 'fedora-fedmsg', ['check1': 'value1'])
-//job.addGit([repo_branch: 'master', repo_url: 'https://github.com/CentOS-PaaS-SIG/contra-env-sample-project.git'])
-job.logRotate()
+job.addGit([repo_branch: 'master', repo_url: 'https://github.com/CentOS-PaaS-SIG/contra-env-sample-project.git'])
+//job.logRotate()
