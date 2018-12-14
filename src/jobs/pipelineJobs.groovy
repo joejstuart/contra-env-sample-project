648731b8559051ee3cc1a465279c9e78e92a3fff
jstuart <jstuart@redhat.com>
2018-12-14 10:12:30 -0600

    adding comment

--- src/jobs/pipelineJobs.groovy
+++ src/jobs/pipelineJobs.groovy
@@ -3,6 +3,7 @@ import org.centos.contra.jobdsl.PipelineJob
 
 def job = new PipelineJob(this, 'samplePipelineJob')
 
+// uncomment if you have fed msg configured
 //job.fedMsgTrigger('org.fedoraprojectb', 'fedora-fedmsg', ['check1': 'value1'])
 job.addGit([repo_branch: 'master', repo_url: 'https://github.com/CentOS-PaaS-SIG/contra-env-sample-project.git'])
 job.logRotate()
